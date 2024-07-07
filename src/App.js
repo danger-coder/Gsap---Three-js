cc7cc80ec87544ff4c31213a866b8232d03ad558
karan chaudhary <karanchy95@gmail.com>
2024-07-07 21:14:32 +0545

    fromTo() fucntion

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
@@ -6,18 +6,25 @@ import gsap from 'gsap';
 function App() {
 
   useGSAP(()=>{
-    gsap.to('#red-box',{
+    gsap.fromTo('#red-box',{
+      x:0,
+      rotation:0,
+   borderRadius:'0%',
+      ease:"elastic.out"
+    },{
       x:250,
       repeat:-1,
-      yoyo:true
+      yoyo:true,
+      borderRadius:'100%',
+      rotation:360,
+      duration:2,
+      ease:"bounce.out"
     })
   },[])
   return (
     <div className="h-screen w-full flex items-center justify-center">
 
       <div id='red-box' className='h-20 w-20 bg-red-700 cursor-pointer'>
-
-
       </div>
      
     </div>
